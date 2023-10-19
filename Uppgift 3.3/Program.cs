1ace69e76ae7c2d4bf32ff1e8adfc5d97f0a13e7
Pingvu <nhlpool27@gmail.com>
2023-10-19 12:27:06 +0200

    Add project files.

---
+++ Uppgift 3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge vill du hyra bilen? (Du får bara låna bilen i maximalt 11 timmar. Skriv också svaret i heltal.");
+            int hyra = int.Parse(Console.ReadLine());
+
+            switch (hyra)
+            {
+                case > 11:
+                    Console.WriteLine("Du får inte hyra bilen så länge.");
+                    break;
+
+                default:
+                    int summa = hyra * 80;
+                    Console.WriteLine("Hyran är " + summa + " kr.");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
